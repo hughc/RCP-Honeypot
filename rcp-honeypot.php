af8eb95c2cc0067fa115432e571c257b3e608294
Hugh Campbell <hc@highbrow.com.au>
2016-11-07 09:49:31 +1100

    rewrote HTML to make honeypot field less obvious to DOM readers

--- rcp-honeypot.php
+++ rcp-honeypot.php
@@ -11,18 +11,23 @@ class RCP_Honeypot {
 	public function __construct() {
 
 		add_action( 'rcp_form_errors',                 array( $this, 'error_checks'   ) );
-		add_action( 'rcp_before_register_form_fields', array( $this, 'honeypot_field' ) );
+		add_action( 'rcp_after_register_form_fields', array( $this, 'honeypot_field' ) );
 
 	}
 
 	public function error_checks() {
-		if( ! empty( $_POST['rcp_sugar_pot'] ) ) {
+		if( ! empty( $_POST['rcp_phone_field_002'] ) ) {
 			rcp_errors()->add( 'spammer', __( 'Nice try spammer, feel free to try again', 'rcp' ), 'register' );
 		}
 	}
 
 	public function honeypot_field() {
-		echo '<input type="hidden" name="rcp_sugar_pot" value=""/>';
+		?>
+		<p id="rcp_validation_wrap" style='display:none; position: absolute!important; left: -9000px;'>
+			<label for="rcp_phone_field_002">Phone</label>
+			<input name="rcp_phone_field_002" id="rcp_phone_field_002" type="text" placeholder="Phone">
+		</p>
+		<?
 	}
 
 }
